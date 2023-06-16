b3cc98474825e6a933e3a852681aebd0773fb923
Lennert Van Geert <lennert.vangeert@gmail.com>
2023-06-16 18:58:42 +0200

    polishing

--- Projects/lightswitch/script.js
+++ Projects/lightswitch/script.js
@@ -1,5 +1,6 @@
 let turnOn = document.getElementById('turnOn');
 let turnOff = document.getElementById('turnOff');
+let btndiv = document.getElementById('btn-div');
 let lamp = document.getElementById('lamp');
 let bg = document.querySelector('body');
 const audio = new Audio('audio/switch_04-43153.mp3');
@@ -9,6 +10,10 @@ function lightOn() {
     lamp.classList.add ("light");
     bg.classList.remove("body-bg-dark");
     bg.classList.add("body-bg-light");
+    turnOn.style.backgroundColor = "rgb(234,233,71)"
+    turnOff.style.backgroundColor = "rgb(200, 194, 194)"
+    btndiv.style.opacity = "100%"
+    console.log("Light on");
 }
 
 function lightOff() {
@@ -16,6 +21,10 @@ function lightOff() {
     lamp.classList.add ("dark");
     bg.classList.remove("body-bg-light");
     bg.classList.add("body-bg-dark");
+    turnOff.style.backgroundColor = "rgb(234,233,71)"
+    turnOn.style.backgroundColor = "rgb(200, 194, 194)"
+    btndiv.style.opacity = "25%"
+    console.log("Light off");
 }
 
 function sound() {
