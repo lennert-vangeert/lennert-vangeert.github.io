9bea4e639449dd03e25c6690587ce6ac0a3c279e
Lennert Van Geert <lennert.vangeert@gmail.com>
2023-07-16 18:05:32 +0200

    added must see

---
+++ Projects/Collab-Project/must-see/script.js
@@ -0,0 +1,32 @@
+var shibuya = document.getElementById('shibuya');
+var body = document.body;
+
+shibuya.onmouseover = function() {
+	body.className = 'shibuya';
+}
+
+shibuya.onmouseout = function() {
+	body.className = '';
+}
+
+var shinjuku = document.getElementById('shinjuku');
+var body = document.body;
+
+shinjuku.onmouseover = function() {
+	body.className = 'shinjuku';
+}
+
+shinjuku.onmouseout = function() {
+	body.className = '';
+}
+
+var asakusa = document.getElementById('asakusa');
+var body = document.body;
+
+asakusa.onmouseover = function() {
+	body.className = 'asakusa';
+}
+
+asakusa.onmouseout = function() {
+	body.className = '';
+}
