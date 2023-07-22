092db1072f0b72461eb408ff2c586890d099f139
Lennert Van Geert <lennert.vangeert@gmail.com>
2023-07-22 11:40:21 +0200

    tried header button

--- Projects/Collab-Project/home-page/js/index.js
+++ Projects/Collab-Project/home-page/js/index.js
@@ -3,24 +3,32 @@ let navbutton = document.getElementById("navbutton");
 let navbutton2 = document.getElementById("navbutton2");
 let navbutton3 = document.getElementById("navbutton3");
 let headerdiv = document.getElementById("headerdiv");
+let mobilemenu = document.getElementById("mobilemenu");
+let btncheck = document.getElementById("mainbutton");
 
+if (w <= 900) {
+  navbutton.remove();
+  navbutton2.remove();
+  navbutton3.remove();
+  let img = document.createElement("img");
+  img.src = "home-page/images/nav.svg";
+  img.id = "mainbutton";
+  headerdiv.appendChild(img);
+} else {
+  document.createElement("navbutton");
+  headerdiv.appendChild(navbutton);
+  document.createElement("navbutton2");
+  headerdiv.appendChild(navbutton2);
+  document.createElement("navbutton3");
+  headerdiv.appendChild(navbutton3);
+}
 
+function createMenu() {
+  if (mobilemenu.className === "mobilemenu hidden") {
+    mobilemenu.className = "mobilemenu";
+  } else {
+    mobilemenu.className = "mobilemenu hidden";
+  }
+}
 
-
-	if (w <= 900) {
-		navbutton.remove();
-		navbutton2.remove();
-        navbutton3.remove();
-		let img = document.createElement("img");
-		img.src = "home-page/images/nav.svg"
-		headerdiv.appendChild(img);
-	} else {
-		document.createElement("navbutton");
-		headerdiv.appendChild(navbutton);
-		document.createElement("navbutton2");
-		headerdiv.appendChild(navbutton2);
-        document.createElement("navbutton3");
-        headerdiv.appendChild(navbutton3);
-	}
-
-
+btncheck.addEventListener("click", createMenu);
