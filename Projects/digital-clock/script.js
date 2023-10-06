e472ff7ef62e6d70b7c9ac57a7cc6751977876e6
Lennert Van Geert <lennert.vangeert@gmail.com>
2023-10-06 13:37:41 +0200

    update clock

--- Projects/digital-clock/script.js
+++ Projects/digital-clock/script.js
@@ -22,21 +22,24 @@ const updateTime = () => {
   const padTime = (time) => {
     return time < 10 ? `0${time}` : `${time}`;
   };
-  const maxTime = (time) => {
-    return time > 23 ? `${time - 24}` : `${time}`;
+  const minMaxTime = (time) => {
+    if (time > 23) {
+      return `${time - 24}`;
+    } else if (time < 0) {
+      return `${time + 24}`;
+    } else {
+      return `${time}`;
+    }
   };
-  const minTime = (time) => {
-    return time < 0 ? `${time + 24}` : `${time}`;
-  }
 
   const giveTime = () => {
-    getBrussels.innerHTML = `<h2>${maxTime(padTime(brusselsTime[0]))}:${padTime(
-      brusselsTime[1]
-    )}:${padTime(brusselsTime[2])}</h2>`;
-    getNewYork.innerHTML = `<h2>${minTime(maxTime(padTime(newYorkTime[0])))}:${padTime(
-      newYorkTime[1]
-    )}:${padTime(newYorkTime[2])}</h2>`;
-    tokyo.innerHTML = `<h2>${minTime(maxTime(padTime(tokyoTime[0])))}:${padTime(
+    getBrussels.innerHTML = `<h2>${minMaxTime(
+      padTime(brusselsTime[0])
+    )}:${padTime(brusselsTime[1])}:${padTime(brusselsTime[2])}</h2>`;
+    getNewYork.innerHTML = `<h2>${minMaxTime(
+      padTime(newYorkTime[0])
+    )}:${padTime(newYorkTime[1])}:${padTime(newYorkTime[2])}</h2>`;
+    tokyo.innerHTML = `<h2>${minMaxTime(padTime(tokyoTime[0]))}:${padTime(
       tokyoTime[1]
     )}:${padTime(tokyoTime[2])}</h2>`;
   };
