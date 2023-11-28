34d8ac93c8511c11451ad165a2141c8a3a17f424
Lennert Van Geert <lennert.vangeert@gmail.com>
2023-11-28 13:22:08 +0100

    added comments

--- Projects/animation-on-scroll/script.js
+++ Projects/animation-on-scroll/script.js
@@ -14,12 +14,15 @@ const initHead = () => {
   }
 };
 initHead();
+// initializes the head and preloads all the images
+
 
 if (localStorage.getItem("scrollPercent")) {
   window.scrollTo(0, localStorage.getItem("scrollPercent"));
 } else {
   window.scrollTo(0, 0);
 }
+// if the user has scrolled before, scroll to that position, otherwise scroll to the top
 
 const getScrollPercent = () => {
   const h = document.documentElement;
@@ -28,6 +31,7 @@ const getScrollPercent = () => {
   const sh = "scrollHeight";
   return ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100;
 };
+// gets the scroll percentage of the page
 
 const setFrame = () => {
   $screen.style.backgroundImage = `url(frames/${Math.floor(
@@ -35,6 +39,7 @@ const setFrame = () => {
   )}.png)`;
   console.log(scrollPercent * 2);
 };
+// sets the frame of the animation according to the scroll percentage
 
 const autoScroll = () => {
   clearTimeout();
@@ -45,13 +50,16 @@ const autoScroll = () => {
     }, 4 * i);
   }
 };
+// sets the scrolllocation to 0, 0 and scrolls the page to the bottom
 
 $btn.addEventListener("click", () => {
   autoScroll();
 });
+// event listener for the button
 
 window.addEventListener("scroll", () => {
   scrollPercent = Math.round(getScrollPercent());
   localStorage.setItem("scrollPercent", scrollPercent);
   setFrame();
 });
+// event listener for the scroll
