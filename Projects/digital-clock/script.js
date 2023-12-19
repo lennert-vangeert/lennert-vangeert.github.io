9a25b8b70ba6d209eea3b9e5adaf47974dca09a7
Lennert Van Geert <lennert.vangeert@gmail.com>
2023-12-19 11:40:56 +0100

    added change hour format and work with timezones

--- Projects/digital-clock/script.js
+++ Projects/digital-clock/script.js
@@ -1,51 +1,50 @@
 const getBrussels = document.getElementById("brussels");
 const getNewYork = document.getElementById("newyork");
-const tokyo = document.getElementById("tokyo");
+const getTokyo = document.getElementById("tokyo");
+let twoFourHour = true;
+
+document.getElementById("btn").addEventListener("click", () => {
+  twoFourHour = !twoFourHour;
+  formatTime();
+  updateTime();
+});
+
+
+
+
+const formatTime = (date, timeZone) => {
+  let options;
+  if (twoFourHour === true) {
+  options = {
+    timeZone,
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false,
+  };
+} else {
+  options = {
+    timeZone,
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
+  };
+}
+  return new Intl.DateTimeFormat("en-UK", options).format(date);
+};
 
 const updateTime = () => {
-  const brusselsTime = [
-    new Date().getHours(),
-    new Date().getMinutes(),
-    new Date().getSeconds(),
-  ];
-  const newYorkTime = [
-    new Date().getHours() - 6,
-    new Date().getMinutes(),
-    new Date().getSeconds(),
-  ];
-  const tokyoTime = [
-    new Date().getHours() + 7,
-    new Date().getMinutes(),
-    new Date().getSeconds(),
-  ];
+  const now = new Date();
 
-  const padTime = (time) => {
-    return time < 10 ? `0${time}` : `${time}`;
-  };
-  const minMaxTime = (time) => {
-    if (time > 23) {
-      return `${time - 24}`;
-    } else if (time < 0) {
-      return `${time + 24}`;
-    } else {
-      return `${time}`;
-    }
-  };
+  const brusselsTime = formatTime(now, "Europe/Brussels");
+  const newYorkTime = formatTime(now, "America/New_York");
+  const tokyoTime = formatTime(now, "Asia/Tokyo");
 
-  const giveTime = () => {
-    getBrussels.innerHTML = `<h2>${minMaxTime(
-      padTime(brusselsTime[0])
-    )}:${padTime(brusselsTime[1])}:${padTime(brusselsTime[2])}</h2>`;
-    getNewYork.innerHTML = `<h2>${minMaxTime(
-      padTime(newYorkTime[0])
-    )}:${padTime(newYorkTime[1])}:${padTime(newYorkTime[2])}</h2>`;
-    tokyo.innerHTML = `<h2>${minMaxTime(padTime(tokyoTime[0]))}:${padTime(
-      tokyoTime[1]
-    )}:${padTime(tokyoTime[2])}</h2>`;
-  };
-  giveTime();
+  getBrussels.innerHTML = `<h2>${brusselsTime}</h2>`;
+  getNewYork.innerHTML = `<h2>${newYorkTime}</h2>`;
+  getTokyo.innerHTML = `<h2>${tokyoTime}</h2>`;
 };
+
 updateTime();
-setInterval(() => {
-  updateTime();
-}, 1000);
+setInterval(updateTime, 1000);
