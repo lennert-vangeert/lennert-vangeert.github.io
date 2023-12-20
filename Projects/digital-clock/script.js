449097da716dbb445bbde77a40b5ae3693530aca
Lennert Van Geert <lennert.vangeert@gmail.com>
2023-12-20 12:02:39 +0100

    added ability to choose timezone

--- Projects/digital-clock/script.js
+++ Projects/digital-clock/script.js
@@ -1,7 +1,13 @@
-const getBrussels = document.getElementById("brussels");
-const getNewYork = document.getElementById("newyork");
-const getTokyo = document.getElementById("tokyo");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const third = document.getElementById("third");
 let twoFourHour = localStorage.getItem("twoFourHour") === "true";
+const inputone = document.getElementById("inputone");
+const inputtwo = document.getElementById("inputtwo");
+const inputthree = document.getElementById("inputthree");
+const firstloc = document.getElementById("firstloc");
+const secondloc = document.getElementById("secondloc");
+const thirdloc = document.getElementById("thirdloc");
 
 document.getElementById("btn").addEventListener("click", () => {
   twoFourHour = !twoFourHour;
@@ -9,6 +15,12 @@ document.getElementById("btn").addEventListener("click", () => {
   formatTime();
   updateTime();
 });
+
+initLocalStorage = () => {
+  inputone.value = localStorage.getItem("inputone") ?? "Europe/London";
+  inputtwo.value = localStorage.getItem("inputtwo") ?? "America/New_York";
+  inputthree.value = localStorage.getItem("inputthree") ?? "Asia/Tokyo";
+};
 
 const formatTime = (date, timeZone) => {
   let options;
@@ -32,16 +44,41 @@ const formatTime = (date, timeZone) => {
   return new Intl.DateTimeFormat("en-UK", options).format(date);
 };
 
+inputone.addEventListener("change", () => {
+  localStorage.setItem("inputone", inputone.value);
+  updateTime();
+});
+
+inputtwo.addEventListener("change", () => {
+  localStorage.setItem("inputtwo", inputtwo.value);
+  updateTime();
+});
+
+inputthree.addEventListener("change", () => {
+  localStorage.setItem("inputthree", inputthree.value);
+  updateTime();
+});
+
+const replaceUnderScore = (string) => {
+  return string.replace("_", " ");
+}
+
+
 const updateTime = () => {
   const now = new Date();
+  
+  initLocalStorage();
+  const firstTime = formatTime(now, inputone.value);
+  const secondTime = formatTime(now, inputtwo.value);
+  const thirdTime = formatTime(now, inputthree.value);
 
-  const brusselsTime = formatTime(now, "Europe/Brussels");
-  const newYorkTime = formatTime(now, "America/New_York");
-  const tokyoTime = formatTime(now, "Asia/Tokyo");
+  firstloc.innerHTML = replaceUnderScore(inputone.value.split("/")[inputone.value.split("/").length - 1]);
+  secondloc.innerHTML = replaceUnderScore(inputtwo.value.split("/")[inputtwo.value.split("/").length - 1]);
+  thirdloc.innerHTML = replaceUnderScore(inputthree.value.split("/")[inputthree.value.split("/").length - 1]);
+  first.innerHTML = `<h2>${firstTime}</h2>`;
+  second.innerHTML = `<h2>${secondTime}</h2>`;
+  third.innerHTML = `<h2>${thirdTime}</h2>`;
 
-  getBrussels.innerHTML = `<h2>${brusselsTime}</h2>`;
-  getNewYork.innerHTML = `<h2>${newYorkTime}</h2>`;
-  getTokyo.innerHTML = `<h2>${tokyoTime}</h2>`;
 };
 
 updateTime();
