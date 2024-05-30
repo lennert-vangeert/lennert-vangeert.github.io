f9983b17045b717856a1ed49f5ab1adc5ebcc50c
Lennert Van Geert <lennert.vangeert@gmail.com>
2024-05-30 19:13:02 +0200

    remove again

--- Projects/bergfreunde-webshop/api/src/modules/Likes/Like.types.ts
+++
@@ -1,7 +0,0 @@
-import { Document, ObjectId } from "mongoose";
-
-export type Like = Document & {
-  _id?: string;
-  userId: ObjectId;
-  productId: ObjectId;
-};
--- Projects/bergfreunde-webshop/api/src/validation/isValidEmail.ts
+++
@@ -1,7 +0,0 @@
-const isValidEmail = (email: string) => {
-    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return regex.test(email);
-  };
-  
-  export default isValidEmail;
-  
