86fdb344eba75a4a0c17adbaf8ef8aab7cac8e10
Lennert Van Geert <lennert.vangeert@gmail.com>
2024-07-16 11:47:34 +0200

    added salary calculator project

---
+++ Projects/salary-calculator/src/Calculator/Calculator.jsx
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from "react";
+import style from "./Calculator.module.css";
+
+function Exercise() {
+  const [form, setForm] = useState({
+    bruto: 0,
+    traffic: 0,
+    meal: 0,
+    bonus: 0,
+  });
+
+  const [total, setTotal] = useState(0);
+
+  const workDays = 5;
+  const workHours = workDays * 7.75;
+
+  const currencyFormatter = new Intl.NumberFormat("nl-BE", {
+    style: "currency",
+    currency: "EUR",
+  });
+
+  useEffect(() => {
+    const storedForm = JSON.parse(localStorage.getItem("formValues"));
+    if (storedForm) {
+      setForm(storedForm);
+      calculateSalary(storedForm);
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: parseFloat(value),
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("formValues", JSON.stringify(form));
+    calculateSalary(form);
+  };
+
+  const calculateSalary = (form) => {
+    if (
+      form.bruto === 0 ||
+      form.traffic === 0 ||
+      form.meal === 0 ||
+      form.bonus === 0
+    ) {
+      return;
+    }
+    const traffic = form.traffic * workDays;
+    const meal = (form.meal - 1.09) * workDays;
+    const bonus = form.bonus * workHours;
+    const salary = form.bruto * workHours;
+    const totalSalary = salary + traffic + meal + bonus;
+    setTotal(totalSalary);
+  };
+
+  return (
+    <div className={style.main}>
+      <h1>Loon rekenmachine (jobstudenten)</h1>
+      <form className={style.form} onSubmit={handleSubmit}>
+        <label htmlFor="bruto">
+          Bruto loon (per uur)
+          <input
+            required
+            type="number"
+            name="bruto"
+            step="0.01"
+            value={form.bruto}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="traffic">
+          Woon-werkverkeer (per dag)
+          <input
+            required
+            type="number"
+            name="traffic"
+            step="0.01"
+            value={form.traffic}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="meal">
+          Maaltijdcheques (per dag)
+          <input
+            required
+            type="number"
+            name="meal"
+            step="0.01"
+            value={form.meal}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="bonus">
+          Toeslagen (per uur)
+          <input
+            required
+            type="number"
+            name="bonus"
+            step="0.01"
+            value={form.bonus}
+            onChange={handleChange}
+          />
+        </label>
+        <button className={style.button} type="submit">
+          Bereken
+        </button>
+      </form>
+      <div className={style.results}>
+        <h1>Resultaten</h1>
+        <h2>Wekelijkse loon</h2>
+        <p className={style.result}>{currencyFormatter.format(total)}</p>
+        <h2>Maandelijkse loon</h2>
+        <p className={style.result}>{currencyFormatter.format(total * 4)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Exercise;
